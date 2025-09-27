b18e05a00dee8097d218dc393c40a6bd039a7644
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-09-27 15:40:46 -0700

    clearer solution 2nd time around

--- 2-Verilog Language/3-Modules/28-moduleaddsub.v
+++ 2-Verilog Language/3-Modules/28-moduleaddsub.v
@@ -20,3 +20,19 @@ module top_module(
     assign sum[15:0]=wtop;
     assign sum[31:16]=wbottom;    
 endmodule
+
+/* Clearer solution (second time trying)
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    input sub,
+    output [31:0] sum
+);
+    wire[31:0] bxorsub;
+    wire cout;
+    assign bxorsub = sub? ~b:b; // Can't actually use an XOR gate because of different sizing. Sub most likely gets padded with zeros, which doesn't products the desired effect
+    add16 inst1 (a[15:0], bxorsub[15:0], sub, sum[15:0], cout);
+    add16 inst2 (a[31:16], bxorsub[31:16], cout, sum[31:16]);
+
+endmodule
+*/
