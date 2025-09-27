d1848104d8f0688b1dbb5dabe49a8fac7c08d8f2
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-09-27 11:07:20 -0700

    practice round 2

--- 2-Verilog Language/3-Modules/26-modulemodulefadd.v
+++ 2-Verilog Language/3-Modules/26-modulemodulefadd.v
@@ -16,3 +16,24 @@ module add1 ( input a, input b, input cin,   output sum, output cout );
 // Full adder module here
     assign {cout, sum} = a + b + cin;
 endmodule
+
+/* newer solution for practice and fuller explanation
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cout;
+    add16 inst1 (a[15:0], b[15:0], 0, sum[15:0], cout);
+    add16 inst2 (a[31:16], b[31:16], cout, sum[31:16]);
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+// Full adder module here
+    wire [1:0] w = a+b+cin;
+    assign sum = w[0]; // last binary digit
+    assign cout = w[1]; // if sum ends up taking up 2 digits
+
+endmodule
+*/
