49288463328589d4c654b198ccbbc56a7fffe767
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-09-27 12:49:34 -0700

    practice round 2

--- 2-Verilog Language/3-Modules/27-modulecseladd.v
+++ 2-Verilog Language/3-Modules/27-modulecseladd.v
@@ -19,3 +19,22 @@ module top_module(
                      assign sum[15:0]=wtop;
                      assign sum[31:16]=wbottom;
 endmodule
+
+/* Alternate solution, possibly better since less wires
+
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cout;
+    wire [15:0] sum1, sum2;
+	
+    add16 first (a[15:0],b[15:0],0,sum[15:0],cout);
+    
+    add16 second1 (a[31:16], b[31:16], 0, sum1);
+    add16 second2 (a[31:16], b[31:16], 1, sum2);
+    assign sum[31:16] = cout? sum2:sum1;
+
+endmodule 
+*/
