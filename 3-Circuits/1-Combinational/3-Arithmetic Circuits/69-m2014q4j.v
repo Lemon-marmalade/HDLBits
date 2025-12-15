52ad4368f01c8ed3ee084969f8fe2f826135b217
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-15 10:07:42 -0800

    Trying generate for loops

--- 3-Circuits/1-Combinational/3-Arithmetic Circuits/69-m2014q4j.v
+++ 3-Circuits/1-Combinational/3-Arithmetic Circuits/69-m2014q4j.v
@@ -12,3 +12,32 @@ endmodule
 module fadd (input x, y, cin, output cout, sum);
     assign {cout,sum} = x+y+cin;
 endmodule
+
+/* using generate loop
+
+module top_module (
+    input [3:0] x,
+    input [3:0] y, 
+    output [4:0] sum);
+    wire cin[3:1];
+    
+    // First one is special
+    FA u1(x[0],y[0],0,cin[1], sum[0]);
+    
+    genvar i;
+    generate
+        for (i=1; i<3; i++) begin: FAgenerate
+            FA ui (x[i],y[i],cin[i],cin[i+1], sum[i]);
+        end
+    endgenerate
+    // Last one is also special.. in hindsight, not much work is saved using generate
+    FA i4(x[3],y[3],cin[3],sum[4], sum[3]);
+endmodule
+
+module FA( 
+    input a, b, cin,
+    output cout, sum );
+    assign {cout,sum} = a+b+cin;
+endmodule
+
+*/
