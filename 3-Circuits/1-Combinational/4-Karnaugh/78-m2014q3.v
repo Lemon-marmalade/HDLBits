f6f22fc87a0ded89766cf7adf9d013c31d35dbed
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-25 11:50:53 -0800

    continued practice

--- 3-Circuits/1-Combinational/4-Karnaugh/78-m2014q3.v
+++ 3-Circuits/1-Combinational/4-Karnaugh/78-m2014q3.v
@@ -3,3 +3,16 @@ module top_module (
     output f );
     assign f = (~x[1]|~x[3])&(x[3]|x[1])&(x[3]|x[2]); // used POS for extra practice w it
 endmodule
+
+/* Practicing again with both SOP and POS
+
+module top_module (
+    input [4:1] x, 
+    output f );
+    // sop
+    //assign f = (x[3]&~x[1]) | (x[2]&x[4]);
+    //pos
+    assign f = (x[3]|x[4]) & (~x[1]|x[2]) & (x[1]|x[3]) & (~x[1]|~x[3]);
+endmodule
+
+*/
