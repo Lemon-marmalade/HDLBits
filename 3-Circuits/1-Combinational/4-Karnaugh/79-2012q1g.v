0311a241370e78ac3f50724d5bd325f0ff8366fe
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-25 11:58:38 -0800

    revisiting SOP and POS

--- 3-Circuits/1-Combinational/4-Karnaugh/79-2012q1g.v
+++ 3-Circuits/1-Combinational/4-Karnaugh/79-2012q1g.v
@@ -5,3 +5,18 @@ module top_module (
     assign f = (~x[1]&x[3])|(x[3]&x[4]&x[2])|(~x[4]&~x[2]); // SOP form
 
 endmodule
+
+/* Practicing this question againwith both sop and pos
+
+module top_module (
+    input [4:1] x,
+    output f
+); 
+    // sop
+    //assign f = (~x[2]&~x[3]&~x[4]) | (x[1]&~x[2]&~x[4]) | (~x[1]&x[3]) | (x[2]&x[3]&x[4]);
+    //pos
+    assign f = (~x[1]|~x[2]|x[4]) & (x[3]|~x[4]) & (~x[2]|x[3]) & (~x[1]|x[2]|~x[4]);
+
+endmodule
+
+*/
