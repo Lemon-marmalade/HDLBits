c53bb4ec95265a11aa42aa118808955b7d364605
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-26 19:37:24 -0800

    JK flip flop revisiting
    Added a new module structure for a JK flip-flop with clock input.

--- 3-Circuits/2-Sequential/1-Latches and Flip Flops/94-ece2412013q7.v
+++ 3-Circuits/2-Sequential/1-Latches and Flip Flops/94-ece2412013q7.v
@@ -16,3 +16,20 @@ module top_module (
     end
 
 endmodule
+
+/* Different structure second time around
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+    always @(posedge clk) begin
+        case({j,k})
+            2'b01: Q<=1'b0;
+            2'b10: Q<=1'b1;
+            2'b11: Q<=~Q;
+        endcase
+    end
+endmodule
+
+*/
