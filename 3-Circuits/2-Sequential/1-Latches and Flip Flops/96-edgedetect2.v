a9933c5428c575533a485d3dfe35f789ed6e21b7
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-26 19:49:26 -0800

    Include alternative edge detection module format
    Added alternative module format for edge detection.

--- 3-Circuits/2-Sequential/1-Latches and Flip Flops/96-edgedetect2.v
+++ 3-Circuits/2-Sequential/1-Latches and Flip Flops/96-edgedetect2.v
@@ -10,3 +10,21 @@ module top_module (
     end
         
 endmodule
+
+
+/* Different format
+
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg[7:0] previn;
+    always @(posedge clk) begin
+        previn<=in;
+        anyedge<= (~previn & in) | (previn & ~in);
+    end
+    
+endmodule
+
+*/
