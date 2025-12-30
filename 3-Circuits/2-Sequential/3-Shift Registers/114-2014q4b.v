73a0ffff72ce96b4f1fcc6523c1b20dbd67e9487
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-29 17:50:47 -0800

    Revisiting MUXDFF question and revise for better concision
    Revised version for slightly more concision, added top_module and MUXDFF modules.

--- 3-Circuits/2-Sequential/3-Shift Registers/114-2014q4b.v
+++ 3-Circuits/2-Sequential/3-Shift Registers/114-2014q4b.v
@@ -19,3 +19,26 @@ module MUXDFF (input clk, w, R, E, L, output Q);
         Q <= d;
     end
 endmodule
+
+/* Revised version for slightly more concision
+
+module top_module (
+    input [3:0] SW,
+    input [3:0] KEY,
+    output [3:0] LEDR
+); //
+    
+    MUXDFF u3 (KEY[3], KEY[1], SW[3], KEY[2], KEY[0], LEDR[3]);
+    MUXDFF u2 (LEDR[3], KEY[1], SW[2], KEY[2], KEY[0], LEDR[2]);
+    MUXDFF u1 (LEDR[2], KEY[1], SW[1], KEY[2], KEY[0], LEDR[1]);
+    MUXDFF u0 (LEDR[1], KEY[1], SW[0], KEY[2], KEY[0], LEDR[0]);
+
+endmodule
+
+module MUXDFF (input w, E, R, L, clk, output q);
+    always @(posedge clk) begin
+        q<= L? R:(E? w:q);
+    end
+endmodule
+
+*/
