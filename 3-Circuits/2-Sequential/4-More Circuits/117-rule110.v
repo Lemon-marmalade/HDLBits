4f20596712b76e11fc3b464202eff64bfea4e380
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-29 20:14:50 -0800

    revisiting rule 110

--- 3-Circuits/2-Sequential/4-More Circuits/117-rule110.v
+++ 3-Circuits/2-Sequential/4-More Circuits/117-rule110.v
@@ -17,3 +17,31 @@ module top_module(
     end
 
 endmodule
+
+/* Redid this question while practicing my minimal POS
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q
+); 
+    always @(posedge clk) begin
+        if (load)
+            q<=data;
+        else begin
+            int i;
+            //From kmap (POS): left|center & ~left|~center|~right
+
+            
+            // edge cases
+            q[0]<=(1'b0|q[0]) & (~1'b0|~q[0]|~q[1]);
+            q[511]<=(q[510]|q[511]) & (~q[510]|~q[511]|~1'b0);
+            
+            for (i=1;i<511;i++) begin
+                q[i]<=(q[i-1]|q[i]) & (~q[i-1]|~q[i]|~q[i+1]);
+            end
+        end
+    end
+
+endmodule
+*/
