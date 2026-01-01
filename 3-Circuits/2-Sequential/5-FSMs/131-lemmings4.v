b0b82e0e2bea147e4423e230e623e08091ef8224
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2025-12-31 18:56:32 -0800

    Improve formatting and structure of Lemmings 4 FSM
    Refactor Lemmings 4 FSM for better format and cohesion.

--- 3-Circuits/2-Sequential/5-FSMs/131-lemmings4.v
+++ 3-Circuits/2-Sequential/5-FSMs/131-lemmings4.v
@@ -82,3 +82,56 @@ module top_module(
     end
 
 endmodule
+
+/* Fixed Lemmings 4 (for much better format and cohesion)
+
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    input dig,
+    output walk_left,
+    output walk_right,
+    output aaah,
+    output digging ); 
+    
+	parameter LEFT=0, RIGHT=1, FALL_L=2, FALL_R=3, DIG_L=4, DIG_R=5, SPLAT=6;
+    reg [2:0] state, next_state;
+	integer fall_duration;
+    always @(*) begin
+        // State transition logic
+        case (state)
+            LEFT: next_state = ground? (dig? DIG_L:(bump_left? RIGHT:LEFT)):FALL_L;
+            RIGHT: next_state = ground? (dig? DIG_R:(bump_right? LEFT:RIGHT)):FALL_R;
+            FALL_L: next_state = ground? ((fall_duration>=21)? SPLAT:LEFT):FALL_L;
+            FALL_R: next_state = ground? ((fall_duration>=21)? SPLAT:RIGHT):FALL_R;
+            DIG_L: next_state = ground? DIG_L:FALL_L;
+            DIG_R: next_state = ground? DIG_R:FALL_R;
+            SPLAT: next_state = SPLAT;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if (areset)
+            state <= LEFT;
+        else begin
+            state <= next_state;
+            if (ground)
+            	fall_duration <= 0;
+            else
+                fall_duration <= fall_duration + 1;
+        end
+    end
+
+    // Output logic
+    assign walk_left = (state == LEFT);
+    assign walk_right = (state == RIGHT);
+    assign aaah = (state == FALL_L | state == FALL_R);
+    assign digging = (state == DIG_L | state == DIG_R);
+
+endmodule
+
+*/
