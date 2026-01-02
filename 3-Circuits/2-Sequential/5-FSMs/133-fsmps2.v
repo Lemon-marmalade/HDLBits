0bbb6d1b08f63985291fe0ddc7f8fbe3d2624bc9
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-01 18:07:38 -0800

    Trying alternative logic
    Added an alternative FSM implementation with comments.

--- 3-Circuits/2-Sequential/5-FSMs/133-fsmps2.v
+++ 3-Circuits/2-Sequential/5-FSMs/133-fsmps2.v
@@ -25,3 +25,37 @@ module top_module(
     assign done = (state==DONE);
 
 endmodule
+
+/* Slightly different logic with same result
+
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,    // Synchronous reset
+    output done); //
+    
+    parameter WAITING=0, BYTE1=1, BYTE2=2, BYTE3=3;
+    reg[1:0] state, next_state;
+
+    // State transition logic (combinational)
+    always @(*) begin
+        case(state)
+            WAITING: next_state = in[3]? BYTE1:WAITING;
+            BYTE1: next_state = BYTE2;
+            BYTE2: next_state = BYTE3;
+            BYTE3: next_state = in[3]? BYTE1:WAITING;
+        endcase
+    end
+    // State flip-flops (sequential)
+    always @(posedge clk) begin
+        if (reset)
+            state<=WAITING;
+        else
+            state<=next_state;
+    end
+    // Output logic
+    assign done = (state==BYTE3);
+endmodule
+
+
+*/
