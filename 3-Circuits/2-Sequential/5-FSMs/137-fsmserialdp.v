597d26b9e853a1b85e9c279ee6f6c9e81cdcdd6e
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-02 12:01:17 -0800

    Change parity check FSM to be more concise

--- 3-Circuits/2-Sequential/5-FSMs/137-fsmserialdp.v
+++ 3-Circuits/2-Sequential/5-FSMs/137-fsmserialdp.v
@@ -46,3 +46,54 @@ module top_module(
             
 endmodule
 
+/* Edited parity check version
+
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+); //
+
+    // Modify FSM and datapath from Fsm_serialdata
+	parameter IDLE=0, DATA=1, PAR=2, STOP=3, ERROR=4;
+    reg [2:0] state, next_state;
+    integer bitcount=0;
+    
+    // New: Add parity checking.
+    reg odd_parity;
+    parity check(clk, (reset|state!=DATA), in, odd_parity);
+    
+    always @(*) begin
+        case(state)
+            IDLE: next_state = in? IDLE:DATA;
+            DATA: next_state = (bitcount==8)? ((in == ~odd_parity)? PAR:ERROR):DATA; // 8th bit is parity bit. 
+            //Parity bit is not the last bit, so it must make odd_parity false. 
+            //However, parity module has not yet incorporated the new input bit at this stage so a manual comparison must be made.
+            PAR: next_state = in? STOP:ERROR; // 9th bit is stop bit
+            STOP: next_state = in? IDLE:DATA;
+            ERROR: next_state = in? IDLE:ERROR;
+        endcase
+    end
+    always @(posedge clk) begin
+        if (reset) begin
+            state<=IDLE;
+            bitcount<=0;
+        end
+        else begin
+            state<=next_state;
+            if (state==DATA) begin
+                // New: Datapath to latch input bits.
+                out_byte[bitcount]<=in;
+                bitcount<=bitcount+1;
+            end
+            else
+                bitcount<=0;
+        end
+    end
+    assign done = (state==STOP);
+
+endmodule
+
+*/
