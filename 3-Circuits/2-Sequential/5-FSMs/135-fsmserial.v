8aee1a7c49dd78cca1412cbed16c93f7fa9a322b
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-02 09:50:49 -0800

    Refactor FSM implementation to reduce states
    Rewritten FSM to reduce the number of states and improve logic.

--- 3-Circuits/2-Sequential/5-FSMs/135-fsmserial.v
+++ 3-Circuits/2-Sequential/5-FSMs/135-fsmserial.v
@@ -32,6 +32,44 @@ module top_module(
             else
                 count_bit <= 0;
         end
+
+    /* Rewritten to have one less state
+
+    module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output done
+); 
+    parameter IDLE=0, DATA=1, STOP=2, ERROR=3;
+    reg [1:0] state, next_state;
+    integer bitcount=0;
+    always @(*) begin
+        case(state)
+            IDLE: next_state = in? IDLE:DATA;
+            DATA: next_state = (bitcount>=8)? (in? STOP:ERROR):DATA;
+            STOP: next_state = in? IDLE:DATA;
+            ERROR: next_state = in? IDLE:ERROR;
+        endcase
+    end
+    always @(posedge clk) begin
+        if (reset) begin
+            state<=IDLE;
+            bitcount<=0;
+        end
+        else begin
+            state<=next_state;
+            if (state==DATA)
+                bitcount<=bitcount+1;
+            else
+                bitcount<=0;
+        end
+    end
+    assign done = (state==STOP);
+
+endmodule
+
+*/
     // output
     assign done = (state==DONE);
 
