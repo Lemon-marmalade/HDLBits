0b2b33f042fb656b92a435df8128b4e9b2f1f1b0
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-02 09:54:53 -0800

    Implement reduced-state FSM with data output
    Added a reduced-state version of the FSM with data output functionality.

--- 3-Circuits/2-Sequential/5-FSMs/136-fsmserialdata.v
+++ 3-Circuits/2-Sequential/5-FSMs/136-fsmserialdata.v
@@ -42,3 +42,46 @@ module top_module(
     assign done = (state==DONE);
 
 endmodule
+
+/* reduced-state version with data output
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+); //
+
+    // Use FSM from Fsm_serial
+    parameter IDLE=0, DATA=1, STOP=2, ERROR=3;
+    reg [1:0] state, next_state;
+    integer bitcount=0;
+    always @(*) begin
+        case(state)
+            IDLE: next_state = in? IDLE:DATA;
+            DATA: next_state = (bitcount>=8)? (in? STOP:ERROR):DATA;
+            STOP: next_state = in? IDLE:DATA;
+            ERROR: next_state = in? IDLE:ERROR;
+        endcase
+    end
+    always @(posedge clk) begin
+        if (reset) begin
+            state<=IDLE;
+            bitcount<=0;
+        end
+        else begin
+            state<=next_state;
+            if (state==DATA) begin
+                // New: Datapath to latch input bits.
+                out_byte[bitcount]<=in;
+                bitcount<=bitcount+1;
+            end
+            else
+                bitcount<=0;
+        end
+    end
+    assign done = (state==STOP);
+
+
+endmodule
+*/
