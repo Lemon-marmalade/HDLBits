c6854ce61da4698e92d14bf6e139d49044c10f72
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-03 11:19:49 -0800

    Add decimal version of FSM module as comment
    Added a commented version of the FSM module using decimal numbers for state representation.

--- 3-Circuits/2-Sequential/5-FSMs/143-2014q3bfsm.v
+++ 3-Circuits/2-Sequential/5-FSMs/143-2014q3bfsm.v
@@ -22,3 +22,32 @@ module top_module (
     end
     assign z = ((y==3'b011)||(y==3'b100));
 endmodule
+
+/* Version with decimal numbers instead
+
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input x,
+    output z
+);
+    reg [2:0] y, Y;
+    always @(*) begin
+        case(y)
+            0: Y = x? 1:0;
+            1: Y = x? 4:1;
+            2: Y = x? 1:2;
+            3: Y = x? 2:1;
+            4: Y = x? 4:3;
+        endcase
+    end
+    always @(posedge clk) begin
+        if (reset)
+            y<=0;
+        else
+            y<=Y;
+    end
+    assign z = (y==3 || y==4);
+endmodule
+
+*/
