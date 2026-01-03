b31101af75b7bd02c4819da83038e19309488098
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-03 11:22:14 -0800

    Implement decimal representation for FSM module
    Added a decimal representation version of part c for FSM.

--- 3-Circuits/2-Sequential/5-FSMs/144-2014q3cfsm.v
+++ 3-Circuits/2-Sequential/5-FSMs/144-2014q3cfsm.v
@@ -19,3 +19,28 @@ module top_module (
     assign z = ((y==3'b011)||(y==3'b100));
     assign Y0 = Y;
 endmodule
+
+/* Decimal representation version of part c
+
+module top_module (
+    input clk,
+    input [2:0] y,
+    input x,
+    output Y0,
+    output z
+);
+    reg [2:0] Y;
+    always @(*) begin
+        case(y)
+            0: Y = x? 1:0;
+            1: Y = x? 4:1;
+            2: Y = x? 1:2;
+            3: Y = x? 2:1;
+            4: Y = x? 4:3;
+        endcase
+    end
+    assign Y0 = Y[0];
+    assign z = (y==3 || y==4);
+endmodule
+
+*/
