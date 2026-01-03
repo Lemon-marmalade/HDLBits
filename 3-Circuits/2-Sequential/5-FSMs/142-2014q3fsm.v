db570ecc6ba001a34878d8ff75e3f505e51c78d5
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-03 09:26:29 -0800

    Implement FSM with different method of tracking w each cycle
    Added a new FSM implementation with state handling and output logic.

--- 3-Circuits/2-Sequential/5-FSMs/142-2014q3fsm.v
+++ 3-Circuits/2-Sequential/5-FSMs/142-2014q3fsm.v
@@ -45,3 +45,51 @@ module top_module (
     end
         
 endmodule
+
+/* Different method
+
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input s,
+    input w,
+    output z
+);
+    parameter A=0, B=1;
+    reg state, next_state;
+    reg [1:0] clkcount, wcount;
+    wire [1:0] wnext = wcount + w;   // must have this variable done combinationally or there's delay
+    
+    always @(*) begin
+        case(state)
+            A: next_state = s? B:A;
+            B: next_state = B;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            state<=A;
+            clkcount<=0;
+            wcount<=0;
+            z<=0;
+        end
+        else begin
+            state<=next_state;
+            if (state==B) begin
+                if (clkcount==2) begin
+                    clkcount<=0;
+                    wcount<=0;
+                    z<=(wnext==2);
+                end
+                else begin
+                    clkcount<=clkcount+1;
+                    wcount<=wnext;
+                    z<=0; // z is inactive between windows
+                end
+            end
+        end
+    end
+endmodule
+
+*/
