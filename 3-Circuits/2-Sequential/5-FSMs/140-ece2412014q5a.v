0646c34c2f3ec9957a588ab7098bfa553a3d7b93
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-02 20:13:31 -0800

    Add Moore's version of two's complement FSM
    Implemented Moore's state machine for two's complement logic.

--- 3-Circuits/2-Sequential/5-FSMs/140-ece2412014q5a.v
+++ 3-Circuits/2-Sequential/5-FSMs/140-ece2412014q5a.v
@@ -60,3 +60,38 @@ module top_module (
 endmodule
 
 */
+
+/* Properly implementing Moore's version as required by the question this time
+
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    //to form two’s complement starting from LSB, copy bits unchanged until the first 1 is seen, then invert all remaining
+
+    parameter COPY_0=0, COPY_1=1, INVERT_0=2, INVERT_1=3;
+    reg [1:0] state, next_state;
+    
+    always @(*) begin
+        case (state)
+            COPY_0: next_state = x? COPY_1:COPY_0;
+            // first time 1 is seen, copy, then enter invert phase
+            COPY_1: next_state = x? INVERT_1:INVERT_0;
+            INVERT_0: next_state = x? INVERT_1:INVERT_0;
+            INVERT_1: next_state = x? INVERT_1:INVERT_0;
+        endcase
+    end
+    always @(posedge clk or posedge areset) begin
+        if (areset)
+            state<=COPY_0;
+        else
+            state<=next_state;
+    end
+    
+    assign z = (state==COPY_1 || state==INVERT_0);
+
+endmodule
+
+*/
