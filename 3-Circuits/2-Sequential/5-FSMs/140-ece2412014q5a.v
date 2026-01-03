7f7d3877520c5ab2ff3f4d6714c3c8925170857f
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-02 19:06:48 -0800

    Refactor FSM module with explanations and clearer names
    Revised FSM module with improved naming and added comments for clarity.

--- 3-Circuits/2-Sequential/5-FSMs/140-ece2412014q5a.v
+++ 3-Circuits/2-Sequential/5-FSMs/140-ece2412014q5a.v
@@ -27,3 +27,36 @@ module top_module (
         end
     end
 endmodule
+
+/* Revisited version with explanation, better concision, and clearer naming
+
+module top_module (
+    input clk,
+    input areset,
+    input x,
+    output z
+); 
+    //to form two’s complement starting from LSB, copy bits unchanged until the first 1 is seen, then invert all remaining
+
+    parameter ZERO=0, INVERT=1;
+    reg state, next_state;
+    always @(*) begin
+        case (state)
+            ZERO: next_state = x? INVERT:ZERO;
+            INVERT: next_state = INVERT;
+        endcase
+    end
+    always @(posedge clk or posedge areset) begin
+        if (areset) begin
+            state<=ZERO;
+            z<=0;
+        end
+        else begin
+            state<=next_state;
+            z<=(state==ZERO)? (x? 1:0):(x? 0:1); // else if INVERT, output is inverted
+        end
+    end
+
+endmodule
+
+*/
