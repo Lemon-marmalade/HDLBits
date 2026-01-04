47d78abf8025b066f20755a24c99e31fd78a15e2
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-03 21:10:31 -0800

    Implement 100-bit adder using generate for-loop
    Added a top-level module with a generate for-loop for 100-bit addition and defined a full adder module.

--- 2-Verilog Language/5-More Verilog Features/42-adder100i.v
+++ 2-Verilog Language/5-More Verilog Features/42-adder100i.v
@@ -14,3 +14,28 @@ module top_module(
     end
 
 endmodule
+
+/* Using generate for-loop
+
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+
+    fadd u0 (a[0],b[0],cin, cout[0],sum[0]);
+    genvar i;
+    generate
+        for (i=1;i<100;i++) begin: fadd_gen
+            fadd ui (a[i],b[i],cout[i-1],cout[i],sum[i]);
+        end
+    endgenerate
+endmodule
+
+module fadd( 
+    input a, b, cin,
+    output cout, sum );
+    assign {cout,sum} = a+b+cin;
+endmodule
+
+*/
