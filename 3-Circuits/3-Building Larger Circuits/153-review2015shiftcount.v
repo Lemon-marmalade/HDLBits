307314924477e52960b376de34eea6f633ad9cbb
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-04 08:57:15 -0800

    Implement concise shifting and counting method
    Added a more concise method for shifting and counting.

--- 3-Circuits/3-Building Larger Circuits/153-review2015shiftcount.v
+++ 3-Circuits/3-Building Larger Circuits/153-review2015shiftcount.v
@@ -18,3 +18,23 @@ module top_module (
 
 
 endmodule
+
+/* More concise method with concatenation
+
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if (shift_ena)
+            q<={q[2:0],data};
+        else if (count_ena)
+            q<=q-3'b1;
+    end
+
+endmodule
+
+*/
