a648d2caf46a59ee14712552ace631a145fc36c0
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-04 09:16:32 -0800

    Add alternative FSM shift module implementation
    Added a different version of the FSM shift module with comments.

--- 3-Circuits/3-Building Larger Circuits/155-review2015fsmshift.v
+++ 3-Circuits/3-Building Larger Circuits/155-review2015fsmshift.v
@@ -25,3 +25,39 @@ module top_module (
     end
     assign shift_ena = (state != E);
 endmodule
+
+/* Different version
+
+module top_module (
+    input clk,
+    input reset,      // Synchronous reset
+    output shift_ena);
+    
+    parameter SHIFT=2'b1, DONE=2'd2;
+    reg [1:0] state, next_state;
+    reg [3:0] q;
+    reg [2:0] clkcount;
+    
+    always @(*) begin
+        case(state)
+            SHIFT: next_state = (clkcount==3)? DONE:SHIFT;
+            DONE: next_state = DONE;
+        endcase
+    end
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            state<=SHIFT;
+            clkcount<=3'b0;
+        end
+        else begin
+			state<=next_state;
+            clkcount<=clkcount+3'b1;
+        end
+    end
+    
+    assign shift_ena = (state==SHIFT);
+
+endmodule
+
+*/
