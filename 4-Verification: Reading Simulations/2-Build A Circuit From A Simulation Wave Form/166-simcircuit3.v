a3787665868f7b27a4fba832d040b7ec0c3a6b9d
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-06 13:51:22 -0800

    Improve logic clarity in top_module
    Added a clearer version of the logic in the top_module.

--- 4-Verification: Reading Simulations/2-Build A Circuit From A Simulation Wave Form/166-simcircuit3.v
+++ 4-Verification: Reading Simulations/2-Build A Circuit From A Simulation Wave Form/166-simcircuit3.v
@@ -8,3 +8,18 @@ module top_module (
     assign q = ~(~a&~b)&&~(~c&~d);
 
 endmodule
+
+/* Version with much clearer logic
+
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q );//
+	// b with c or d, a with c or d, b and a can only be together if not alone
+    assign q = (a|b)&(c|d); // Fix me
+
+endmodule
+
+*/
