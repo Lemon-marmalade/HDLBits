39ef72353e673246f57e09b00ad1612fee8207ce
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-06 13:20:24 -0800

    Improve circuit module with updated logic and comments
    Added a better version of the circuit module with comments.

--- 4-Verification: Reading Simulations/2-Build A Circuit From A Simulation Wave Form/165-cimcircuit2.v
+++ 4-Verification: Reading Simulations/2-Build A Circuit From A Simulation Wave Form/165-cimcircuit2.v
@@ -8,3 +8,18 @@ module top_module (
     assign q = (a&b&~c&~d)||(b&c&~d&~a)||(c&d&~a&~b)||(d&a&~b&~c)||(b&d&~a&~c)||(a&c&~b&~d)||(a&b&c&d)||(~a&~b&~c&~d);
 
 endmodule
+
+/* Much better version
+
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q );//
+	// none, all, or in pairs of 2
+    assign q = ~(a^b^c^d); // Fix me
+
+endmodule
+
+*/
