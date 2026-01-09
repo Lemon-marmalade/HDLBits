b83016ec04160156f8cdc146ffab0a9e6a409819
Lemon-marmalade <158791191+Lemon-marmalade@users.noreply.github.com>
2026-01-09 08:58:45 -0800

    Refactor 175-tb1.v for simplicity
    Revised testbench with fewer lines and added comments.

--- 5-Verification: Writing Testbenches/175-tb1.v
+++ 5-Verification: Writing Testbenches/175-tb1.v
@@ -15,3 +15,20 @@ module top_module ( output reg A, output reg B );//
     end
 
 endmodule
+
+/* Revisited version with fewer lines
+
+module top_module ( output reg A, output reg B );//
+
+    // generate input patterns here
+    initial begin
+        A=0; B=0;
+		#10 A=1;
+        #5 B=1;
+        #5 A=0;
+        #20 B=0; // Delays don't need semicolons
+    end
+
+endmodule
+
+*/
